d53da86c8092349a84a05d3175c51e71a129b517
Brandon Smith <88860661+smithbwlhs@users.noreply.github.com>
2025-01-07 14:39:33 -0800

    Create Unit8HW1.java

---
+++ HW1/Unit8HW1.java
@@ -0,0 +1,15 @@
+public class Unit8HW1{
+	public static void main(String[] args) {
+		//make your own 2D test array of integers (or whatever)
+
+		//ask user for column number
+
+		//make sure it is a valid option
+
+		//print the column number they asked for with each element on a new line
+		
+	}
+	public static void columnPrinter(int columnNumber){
+		System.out.println("");
+	}
+}
